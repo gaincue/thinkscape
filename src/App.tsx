3fa3b4cecc850b51a145a8b14165af787c9a1f3e
kkcy <rickykkcy@gmail.com>
2024-05-04 21:28:39 +0800

    vite compiler

---
+++ src/App.tsx
@@ -0,0 +1,504 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import * as THREE from "three"
+import { FC, PropsWithChildren, Suspense, useMemo, useRef } from "react"
+import { Canvas, ReactThreeFiber, useFrame, useThree } from "@react-three/fiber"
+import {
+  AdaptiveDpr,
+  AdaptiveEvents,
+  Center,
+  Environment,
+  // Instance,
+  // Instances,
+  OrbitControls,
+  PerformanceMonitor,
+  Stats,
+  Text3D,
+  useEnvironment,
+  useGLTF,
+  // usePerformanceMonitor,
+} from "@react-three/drei"
+import { CuboidCollider, Physics, RapierRigidBody, RigidBody } from "@react-three/rapier"
+import useSound from "use-sound"
+
+THREE.ColorManagement.enabled = true
+
+// const randomVector = (r: number) => [
+//   r / 2 - Math.random() * r,
+//   r / 2 - Math.random() * r,
+//   r / 2 - Math.random() * r,
+// ]
+// const randomEuler = () => [
+//   Math.random() * Math.PI,
+//   Math.random() * Math.PI,
+//   Math.random() * Math.PI,
+// ]
+
+// const box = new THREE.BoxGeometry()
+// const torus = new THREE.TorusGeometry()
+// const sphere = new THREE.SphereGeometry()
+// const cylinder = new THREE.CylinderGeometry()
+
+// const configPink = {
+//   color: 0xf00074,
+//   transmission: 1.8,
+//   thickness: 100,
+//   roughness: 0,
+//   ior: 2.1,
+//   reflectivity: 0.95,
+//   metalness: 0.25,
+// }
+// const configYellow = {
+//   color: 0xf5cc00,
+//   transmission: 1,
+//   thickness: 80,
+//   roughness: 0,
+//   ior: 2.3,
+//   reflectivity: 1,
+//   metalness: 0,
+// }
+// const configOrange = {
+//   color: 0xf08000,
+//   transmission: 1.6,
+//   thickness: 100,
+//   roughness: 0.1,
+//   ior: 1.7,
+//   reflectivity: 0.7,
+//   metalness: 0.1,
+// }
+// const configBlue = {
+//   color: 0x0034d1,
+//   transmission: 10,
+//   thickness: 150,
+//   roughness: 0,
+//   ior: 2.5,
+//   reflectivity: 0.6,
+//   metalness: 0.8,
+// }
+const materialPink = new THREE.MeshPhysicalMaterial({
+  color: 0xf00074,
+  transmission: 1.8,
+  thickness: 100,
+  roughness: 0,
+  ior: 2.1,
+  reflectivity: 0.95,
+  metalness: 0.25,
+})
+const materialYellow = new THREE.MeshPhysicalMaterial({
+  color: 0xf5cc00,
+  transmission: 1,
+  thickness: 80,
+  roughness: 0,
+  ior: 2.3,
+  reflectivity: 1,
+  metalness: 0,
+})
+const materialOrange = new THREE.MeshPhysicalMaterial({
+  color: 0xf08000,
+  transmission: 1.6,
+  thickness: 100,
+  roughness: 0.1,
+  ior: 1.7,
+  reflectivity: 0.7,
+  metalness: 0.1,
+})
+const materialBlue = new THREE.MeshPhysicalMaterial({
+  color: 0x0034d1,
+  transmission: 10,
+  thickness: 150,
+  roughness: 0,
+  ior: 2.5,
+  reflectivity: 0.6,
+  metalness: 0.8,
+})
+
+function App() {
+  return (
+    <>
+      <Canvas
+        shadows={false}
+        gl={{ antialias: true }}
+        camera={{
+          position: [0, 0, 3],
+        }}
+        frameloop="demand"
+      >
+        <PerformanceMonitor factor={0.5}>
+          <Stats />
+          <color attach="background" args={["#faeb1e"]} />
+          <AdaptiveDpr pixelated />
+          <AdaptiveEvents />
+          <OrbitControls />
+          <Suspense>
+            <TT />
+            <Physics gravity={[0, 0, 0]} colliders={false}>
+              <Pointer />
+              <Model />
+            </Physics>
+          </Suspense>
+        </PerformanceMonitor>
+      </Canvas>
+    </>
+  )
+}
+
+function TT() {
+  const camera = useThree((state) => {
+    console.log(state)
+    return state.camera
+  })
+
+  return (
+    <group position={[-1, 0, 0]} scale={1.5}>
+      <Center>
+        <Text3D
+          size={0.5}
+          height={0.01}
+          quaternion={camera.quaternion}
+          font="/Inter_Bold.json"
+          position={[0, 1.2, 0]}
+        >
+          THINK
+          <meshStandardMaterial color="black" />
+        </Text3D>
+        <Text3D
+          size={0.3}
+          height={0.01}
+          quaternion={camera.quaternion}
+          font="/Inter_Bold.json"
+          position={[0, 0.8, 0]}
+        >
+          OF
+          <meshStandardMaterial color="black" />
+        </Text3D>
+        <Text3D
+          size={0.3}
+          height={0.01}
+          quaternion={camera.quaternion}
+          font="/Inter_Bold.json"
+          position={[0, 0.4, 0]}
+        >
+          SIMPLE
+          <meshStandardMaterial color="black" />
+        </Text3D>
+        <Text3D
+          size={0.4}
+          height={0.01}
+          quaternion={camera.quaternion}
+          font="/Inter_Bold.json"
+          position={[0, -0.1, 0]}
+        >
+          POTENT
+          <meshStandardMaterial color="black" />
+        </Text3D>
+        <Text3D
+          size={0.5}
+          height={0.01}
+          quaternion={camera.quaternion}
+          font="/Inter_Bold.json"
+          position={[0, -0.7, 0]}
+        >
+          IDEAS
+          <meshStandardMaterial color="black" />
+        </Text3D>
+      </Center>
+    </group>
+  )
+}
+
+function Pointer({ vec = new THREE.Vector3() }) {
+  const ref = useRef<RapierRigidBody>(null)
+  useFrame(({ pointer, viewport }) => {
+    ref.current?.setNextKinematicTranslation(
+      vec.set((pointer.x * viewport.width) / 2, (pointer.y * viewport.height) / 2, 0)
+    )
+  })
+
+  return (
+    <RigidBody position={[0, 0, 0]} type="kinematicPosition" colliders={false} ref={ref}>
+      <CuboidCollider args={[0.1, 0.1, 10]} />
+    </RigidBody>
+  )
+}
+
+function Model() {
+  const { scene } = useGLTF("/model.gltf")
+  // const [population, setPopulation] = useState(0.5)
+  // usePerformanceMonitor({
+  //   // onIncline: () => {},
+  //   // onDecline: () => {},
+  //   onChange: ({ factor }) => {
+  //     setPopulation(factor)
+  //   },
+  // })
+
+  const [play] = useSound("/sfx.mp3", { volume: 0.1 })
+
+  /*--- refraction start ---*/
+  const envMap = useEnvironment({
+    files: "/hdri.jpg",
+  })
+  /*--- refraction end ---*/
+
+  const processedScene = useMemo(() => {
+    scene.traverse((child) => {
+      if (child.name.includes("pink")) {
+        // @ts-ignore
+        child.material = materialPink
+        // @ts-ignore
+        child.material.side = THREE.DoubleSide
+      }
+      if (child.name.includes("yellow")) {
+        // @ts-ignore
+        child.material = materialYellow
+        // @ts-ignore
+        child.material.side = THREE.DoubleSide
+      }
+      if (child.name.includes("orange")) {
+        // @ts-ignore
+        child.material = materialOrange
+        // @ts-ignore
+        child.material.side = THREE.DoubleSide
+      }
+      if (child.name.includes("blue")) {
+        // @ts-ignore
+        child.material = materialBlue
+        // @ts-ignore
+        child.material.side = THREE.DoubleSide
+      }
+    })
+
+    return scene
+  }, [scene])
+
+  // const optimizedScene = useMemo(() => {
+  //   const newScene = processedScene.clone()
+  //   newScene.children = newScene.children.slice(0, newScene.children.length * (population / 1))
+  //   return newScene
+  // }, [processedScene, population])
+
+  // return (
+  //   <>
+  //     <Environment map={envMap} />
+
+  //     <group scale={2} position={[-4.2, -3.2, 7.3]}>
+  //       {scene.children.map((child, index) => {
+  //         if (index > 50) {
+  //           return <></>
+  //         }
+
+  //         return (
+  //           <RigidMesh key={child.uuid} position={child.position} playAudio={play}>
+  //             <mesh
+  //               // @ts-ignore
+  //               geometry={child.children[0].geometry}
+  //               position={child.children[0].position}
+  //               rotation={child.rotation}
+  //               scale={child.children[0].scale}
+  //             >
+  //               <meshPhysicalMaterial
+  //                 side={THREE.DoubleSide}
+  //                 {...(child.children[0].name.includes("pink")
+  //                   ? { ...configPink }
+  //                   : child.children[0].name.includes("yellow")
+  //                   ? { ...configYellow }
+  //                   : child.children[0].name.includes("orange")
+  //                   ? { ...configOrange }
+  //                   : { ...configBlue })}
+  //               />
+  //               {/* <MeshTransmissionMaterial
+  //           // samples={10}
+  //           // side={THREE.DoubleSide}
+  //           backside={true}
+  //           resolution={32}
+  //           backsideResolution={32}
+  //           {...(child.children[0].name.includes("pink")
+  //             ? { ...configPink }
+  //             : child.children[0].name.includes("yellow")
+  //             ? { ...configYellow }
+  //             : child.children[0].name.includes("orange")
+  //             ? { ...configOrange }
+  //             : { ...configBlue })}
+  //         /> */}
+  //             </mesh>
+  //           </RigidMesh>
+  //         )
+  //       })}
+  //     </group>
+  //   </>
+  // )
+
+  return (
+    <>
+      <Environment map={envMap} />
+
+      <group scale={2} position={[-4.2, -3.2, 7.3]}>
+        {/* <InstancedBox />
+        <InstancedSphere />
+        <InstancedTorus />
+        <InstancedCylinder /> */}
+        {processedScene.children.map((child) => {
+          return (
+            <RigidMesh key={child.uuid} position={child.position} playAudio={play}>
+              <primitive
+                object={child.children[0]}
+                quarternion={child.quaternion}
+                rotation={child.rotation}
+              />
+            </RigidMesh>
+          )
+        })}
+      </group>
+    </>
+  )
+}
+
+// function InstancedBox() {
+//   const positions = useMemo(
+//     () =>
+//       Array.from({ length: 30 }, (r = 10) => ({
+//         random: Math.random(),
+//         scale: Math.random(),
+//         position: randomVector(r as number),
+//         rotation: randomEuler(),
+//       })),
+//     []
+//   )
+
+//   return (
+//     <Instances
+//       limit={30} // Optional: max amount of items (for calculating buffer size)
+//       range={10} // Optional: draw-range
+//       material={materialYellow}
+//       // @ts-ignore
+//       geometry={box}
+//     >
+//       {positions.map((props, index) => {
+//         return (
+//           // @ts-ignore
+//           <group key={index} {...props}>
+//             <Instance />
+//           </group>
+//         )
+//       })}
+//     </Instances>
+//   )
+// }
+// function InstancedTorus() {
+//   const positions = useMemo(
+//     () =>
+//       Array.from({ length: 30 }, (r = 10) => ({
+//         random: Math.random(),
+//         scale: Math.random(),
+//         position: randomVector(r as number),
+//         rotation: randomEuler(),
+//       })),
+//     []
+//   )
+
+//   return (
+//     <Instances
+//       limit={30} // Optional: max amount of items (for calculating buffer size)
+//       range={10} // Optional: draw-range
+//       material={materialBlue}
+//       // @ts-ignore
+//       geometry={torus}
+//     >
+//       {positions.map((props, index) => {
+//         return (
+//           // @ts-ignore
+//           <group key={index} {...props}>
+//             <Instance />
+//           </group>
+//         )
+//       })}
+//     </Instances>
+//   )
+// }
+// function InstancedSphere() {
+//   const positions = useMemo(
+//     () =>
+//       Array.from({ length: 30 }, (r = 10) => ({
+//         random: Math.random(),
+//         scale: Math.random(),
+//         position: randomVector(r as number),
+//         rotation: randomEuler(),
+//       })),
+//     []
+//   )
+
+//   return (
+//     <Instances
+//       limit={30} // Optional: max amount of items (for calculating buffer size)
+//       range={10} // Optional: draw-range
+//       material={materialOrange}
+//       // @ts-ignore
+//       geometry={sphere}
+//     >
+//       {positions.map((props, index) => {
+//         return (
+//           // @ts-ignore
+//           <group key={index} {...props}>
+//             <Instance />
+//           </group>
+//         )
+//       })}
+//     </Instances>
+//   )
+// }
+// function InstancedCylinder() {
+//   const positions = useMemo(
+//     () =>
+//       Array.from({ length: 30 }, (r = 10) => ({
+//         random: Math.random(),
+//         scale: Math.random(),
+//         position: randomVector(r as number),
+//         rotation: randomEuler(),
+//       })),
+//     []
+//   )
+
+//   return (
+//     <Instances
+//       limit={30} // Optional: max amount of items (for calculating buffer size)
+//       range={10} // Optional: draw-range
+//       // material={materialPink}
+//       // @ts-ignore
+//       geometry={cylinder}
+//     >
+//       <meshPhysicalMaterial side={THREE.DoubleSide} {...configPink} />
+//       {positions.map((props, index) => {
+//         return (
+//           // @ts-ignore
+//           <group key={index} {...props}>
+//             <Instance />
+//           </group>
+//         )
+//       })}
+//     </Instances>
+//   )
+// }
+
+const RigidMesh: FC<
+  PropsWithChildren<{ position: ReactThreeFiber.Vector3; playAudio: () => void }>
+> = ({ children, position, playAudio }) => {
+  const api = useRef<RapierRigidBody>(null)
+  const pos = useMemo(() => position, [position])
+
+  return (
+    <RigidBody
+      ref={api}
+      linearDamping={4}
+      angularDamping={1}
+      friction={0.1}
+      position={pos}
+      colliders="ball"
+      onCollisionEnter={() => {
+        playAudio()
+      }}
+    >
+      {children}
+    </RigidBody>
+  )
+}
+
+export default App
