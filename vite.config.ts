fe5d3063c042d577c5f0cc83d1db019e7dd622b0
kkcy <rickykkcy@gmail.com>
2024-05-08 15:17:11 +0800

    fix: add sfx.mp3 & manual chunk for production

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
-import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
+import { visualizer } from "rollup-plugin-visualizer"
+import { PluginOption, defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer() as PluginOption],
   server: {
     host: true,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.includes(".pnpm/three")) {
+            return "@three"
+          }
+
+          if (id.includes("rapier3d")) {
+            return "@rapier3d"
+          }
+        },
+      },
+    },
   },
 })
